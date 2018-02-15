6b187b1deb41466f52dbadd2264ea068e0c0a655
jimmychimmyy <jimmychimmyy@gmail.com>
2018-02-14 23:23:47 -0800

    implemented projectData.m and recoverData.m

--- KmeansClustering&PCA/ex7/recoverData.m
+++ KmeansClustering&PCA/ex7/recoverData.m
@@ -1,7 +1,7 @@
 function X_rec = recoverData(Z, U, K)
-%RECOVERDATA Recovers an approximation of the original data when using the 
+%RECOVERDATA Recovers an approximation of the original data when using the
 %projected data
-%   X_rec = RECOVERDATA(Z, U, K) recovers an approximation the 
+%   X_rec = RECOVERDATA(Z, U, K) recovers an approximation the
 %   original data that has been reduced to K dimensions. It returns the
 %   approximate reconstruction in X_rec.
 %
@@ -19,9 +19,18 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %                    recovered_j = v' * U(j, 1:K)';
 %
 %               Notice that U(j, 1:K) is a row vector.
-%               
+%
 
 
+%size(X_rec) % 50x2
+%size(Z) % 50x1
+%size(U) % 2x2
+%K % = 1
+
+U_reduce = U(:, 1:K);
+%size(U_reduce)  % 2x1
+
+X_rec = Z * U_reduce';
 
 % =============================================================
 
